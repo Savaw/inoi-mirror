e54808c21d476c45443fa91e198013dbf750fd2c
Grace Hawkins <amoomajid99@gmail.com>
2020-10-08 17:11:21 +0330

    Add date fields of user to django admin

--- cms_register/admin.py
+++ cms_register/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from ordered_model.admin import OrderedModelAdmin
 
 from .models import Announcement, Contest, Participant
 
 
 # Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+	list_display = ('username', 'email', 'first_name', 'last_name', 'date_joined', 'last_login')
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
 
 class AnnAdmin(OrderedModelAdmin):
     fieldsets = [
