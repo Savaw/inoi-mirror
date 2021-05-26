f0120067c9a98a3f28cf9fbf9f7a86e4e4436ce6
Grace Hawkins <amoomajid99@gmail.com>
2021-05-26 13:37:00 +0430

    Add model admin of profile

--- cms_register/admin.py
+++ cms_register/admin.py
@@ -3,17 +3,18 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 from ordered_model.admin import OrderedModelAdmin
 
-from .models import Announcement, Contest, Participant
+from cms_register.models import Announcement, Contest, Participant, Profile
 
-
-# Register your models here.
 
 class UserAdmin(BaseUserAdmin):
-	list_display = ('username', 'email', 'first_name', 'last_name', 'date_joined', 'last_login')
-
-
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'date_joined',
+        'last_login',
+    )
 
 
 class AnnAdmin(OrderedModelAdmin):
@@ -21,9 +22,6 @@ class AnnAdmin(OrderedModelAdmin):
         (None, {'fields': ['announce_text']}),
     ]
     list_display = ('announce_text', 'move_up_down_links')
-
-
-admin.site.register(Announcement, AnnAdmin)
 
 
 class ParLine(admin.TabularInline):
@@ -35,19 +33,35 @@ class ParLine(admin.TabularInline):
 class ConAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['contest_name', 'cms_id']}),
-        ('Times', {'classes': ('collapse', 'open'), 'fields': ['start_time', 'duration', 'contest_time']}),
-        ('Attached',
-         {'classes': ('collapse', 'open'), 'fields': ['contest_url', 'ranking_file', 'unofficial_ranking_file']}),
+        ('Times', {
+            'classes': ('collapse', 'open'),
+            'fields': ['start_time', 'duration', 'contest_time'],
+        }),
+        ('Attached', {
+            'classes': ('collapse', 'open'),
+            'fields': [
+                'contest_url',
+                'ranking_file',
+                'unofficial_ranking_file',
+            ],
+        }),
     ]
-    list_display = ('contest_name', 'start_time', 'duration', 'cms_id', 'reg_count')
-    # inlines = [ParLine]
-
-
-admin.site.register(Contest, ConAdmin)
+    list_display = (
+        'contest_name',
+        'start_time',
+        'duration',
+        'cms_id',
+        'reg_count',
+    )
 
 
 class ParAdmin(admin.ModelAdmin):
     list_display = ('user', 'contest')
 
 
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile)
+admin.site.register(Announcement, AnnAdmin)
+admin.site.register(Contest, ConAdmin)
 admin.site.register(Participant, ParAdmin)
